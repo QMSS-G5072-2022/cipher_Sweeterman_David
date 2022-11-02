f689bc80fee258e8ef574b3dc47a76feb494d5a7
dsweeterman <113548700+dsweeterman@users.noreply.github.com>
2022-11-01 22:38:48 -0400

    added test

--- cipher_dws2138/tests/test_cipher_dws2138.py
+++ cipher_dws2138/tests/test_cipher_dws2138.py
@@ -1,1 +1,7 @@
 from cipher_dws2138 import cipher_dws2138
+
+def test_negative_cipher():
+    example = 'Apple', -2
+    expected = 'ynnjc'
+    actual = cipher_dws2138.cipher('Apple', -2)
+    assert actual == expected
